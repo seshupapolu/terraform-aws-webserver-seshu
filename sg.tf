d8612416dbe3e54d18e03e58e3edec6e9183c605
Seshu Papolu <seshu.papolu@gmail.com>
2023-06-08 15:38:13 +0530

    Base Template

---
+++ sg.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "main" {
+    name        = "EC2-webserver-SG-1"
+  description = "Webserver for EC2 Instances"
+  ingress {
+    from_port   = 80
+    protocol    = "TCP"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    protocol    = "TCP"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
